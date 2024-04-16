a4450c57ffc8e78efa8048aef15b4b2f09543e34
Gargi Joshi <Gargi_Joshi@epam.com>
2024-04-17 02:17:22 +0530

    Added task

---
+++ DesignPatterns/StrategyPatternTask/EmailShareStrategy.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.StrategyPatternTask
+{
+    public class EmailShareStrategy : ISocialMediaStrategy
+    {
+        public void ConnectToSocialMedia(string name)
+        {
+            // Implement email share connect functionality
+            Console.WriteLine("Connecting via Email.....");
+        }
+        public void Share(string message)
+        {
+            // Implement email share functionality
+            Console.WriteLine("Message '" + message + "' shared via Email");
+        }
+    }
+}
---
+++ DesignPatterns/StrategyPatternTask/PhoneCameraApp.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.StrategyPatternTask
+{
+    public class PhoneCameraApp
+    {
+        private ISocialMediaStrategy socialMediaStrategy;
+
+        public void SetSocialMediaStrategy(ISocialMediaStrategy socialMediaStrategy)
+        {
+            this.socialMediaStrategy = socialMediaStrategy;
+        }
+
+        public void Share(String message)
+        {
+            this.socialMediaStrategy.Share(message);
+        }
+
+        // Other methods...
+    }
+}
